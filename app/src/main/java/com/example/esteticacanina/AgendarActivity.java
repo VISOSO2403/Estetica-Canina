36193389e3ae63c142248e50e4f0c22a2d0b9af0
Josselin Valdez <jvaldez13@ucol.mx>
2022-10-19 11:18:09 -0500

    xml fragment user completo

--- app/src/main/java/com/example/esteticacanina/AgendarActivity.java
+++ app/src/main/java/com/example/esteticacanina/AgendarActivity.java
@@ -2,13 +2,32 @@ package com.example.esteticacanina;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class AgendarActivity extends AppCompatActivity {
+public class AgendarActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button cancelar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agendar);
+        cancelar = (Button) findViewById(R.id.btnCancelar);
+
+        cancelar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnCancelar:
+                Intent a = new Intent(AgendarActivity.this, MenuActivity.class);
+                startActivity(a);
+                //Finaliza la pantalla para no generar más memoria
+                finish();
+        }
     }
 }
