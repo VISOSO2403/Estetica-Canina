1419f9fb0f8cd8c12ede76be191a8f6cc11c4b8e
Oliver Visoso <ovisoso@ucol.mx>
2022-10-19 05:38:49 -0500

    Cambio de Activity a fragment Servicios

--- app/src/main/java/com/example/esteticacanina/HomeFragment.java
+++ app/src/main/java/com/example/esteticacanina/HomeFragment.java
@@ -1,41 +1,32 @@
 package com.example.esteticacanina;
 
-import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+public class HomeFragment extends Fragment {
 
-
-public class HomeFragment extends Fragment implements View.OnClickListener{
+    //VARIABLES
     Button vermas;
-    View vista;
+    ServiciosFragment serviciosFragment = new ServiciosFragment();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        vista = inflater.inflate(R.layout.fragment_home, container, false);
+       View root = inflater.inflate(R.layout.fragment_home, container, false);
 
-        vermas = (Button) vista.findViewById(R.id.btnVerMas);
-        vermas.setOnClickListener(this);
+       vermas = root.findViewById(R.id.btnVerMas);
 
-        return vista;
+       vermas.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               getChildFragmentManager().beginTransaction().replace(R.id.contenedor, serviciosFragment).commit();
+           }
+        });
 
-
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.btnVerMas:
-                Intent i = new Intent(getActivity(), ServiciosActivity.class);
-                startActivity(i);
-        }
+        return root;
     }
 }
---
+++ app/src/main/java/com/example/esteticacanina/ServiciosFragment.java
@@ -0,0 +1,28 @@
+package com.example.esteticacanina;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class ServiciosFragment extends Fragment {
+
+    //VARIABLES
+    Button agendar, cancelar;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_servicios, container, false);
+
+
+
+        return root;
+    }
+
+
+}
