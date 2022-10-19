853f0629f43350d0453d6250df49a9be203b070f
Alejandro Escobar <fescobar0@ucol.mx>
2022-10-19 12:31:15 -0500

    Regrese cambios del java

--- app/src/main/java/com/example/esteticacanina/UserFragment.java
+++ app/src/main/java/com/example/esteticacanina/UserFragment.java
@@ -14,4 +14,10 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class UserFragment extends Fragment {
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_user, container, false);
+    }
+
     }
