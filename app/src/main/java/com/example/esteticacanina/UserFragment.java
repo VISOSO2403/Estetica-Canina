09fb78fc1af3c38b2bde2095b1436c78a7017d93
Ivanna1320 <itorres13@ucol.mx>
2022-11-21 16:16:55 -0600

    Funcion cerrar sesión completa

--- app/src/main/java/com/example/esteticacanina/UserFragment.java
+++ app/src/main/java/com/example/esteticacanina/UserFragment.java
@@ -8,21 +8,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class UserFragment extends Fragment {
 
-    Button agremasc;
-
+    Button agremasc, cerrarSesion;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
     @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_user, container, false);
 
+        //Variables de usuario
+        mAuth = FirebaseAuth.getInstance();
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
 
+        //Variables de botones
         agremasc = root.findViewById(R.id.btnagrmasc);
+        cerrarSesion = root.findViewById(R.id.btncerrarsesion);
 
         agremasc.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,7 +42,22 @@ public class UserFragment extends Fragment {
             }
         });
 
+        //Acción de Botón Cerrar Sesión
+        cerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CerrarSesion ();
+            }
+        });
+
         return root;
+    }
+
+    //Metodo Cerrar Sesión
+    private void CerrarSesion (){
+        mAuth.signOut();
+        Toast.makeText(getActivity(), "Sesión Cerrada", Toast.LENGTH_LONG).show();
+        startActivity(new Intent(getActivity(),LoginActivity.class));
     }
 
 }
