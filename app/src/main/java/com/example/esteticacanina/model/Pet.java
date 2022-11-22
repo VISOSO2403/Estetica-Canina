ee754868be89312d2ede91bd0c6782eeb74e6de8
Oliver Visoso <ovisoso@ucol.mx>
2022-11-22 14:59:45 -0600

    Tarjeta Pet funcional

--- app/src/main/java/com/example/esteticacanina/model/Pet.java
+++ app/src/main/java/com/example/esteticacanina/model/Pet.java
@@ -1,4 +1,64 @@
 package com.example.esteticacanina.model;
 
 public class Pet {
+
+    String Edad, Nombre, Peso, Sexo, Tipo, tamano;
+    public Pet(){}
+
+    public Pet(String edad, String nombre, String peso, String sexo, String tipo, String tamano) {
+        Edad = edad;
+        Nombre = nombre;
+        Peso = peso;
+        Sexo = sexo;
+        Tipo = tipo;
+        this.tamano = tamano;
+    }
+
+    public String getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(String edad) {
+        Edad = edad;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getPeso() {
+        return Peso;
+    }
+
+    public void setPeso(String peso) {
+        Peso = peso;
+    }
+
+    public String getSexo() {
+        return Sexo;
+    }
+
+    public void setSexo(String sexo) {
+        Sexo = sexo;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String tipo) {
+        Tipo = tipo;
+    }
+
+    public String getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(String tamano) {
+        this.tamano = tamano;
+    }
 }
