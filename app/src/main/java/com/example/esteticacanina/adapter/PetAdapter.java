2aaf8e56ef4474909ee9fe35c01d5de555b7ab11
Oliver Visoso <ovisoso@ucol.mx>
2022-11-23 07:38:31 -0600

    Errores en la vista

--- app/src/main/java/com/example/esteticacanina/adapter/PetAdapter.java
+++ app/src/main/java/com/example/esteticacanina/adapter/PetAdapter.java
@@ -1,19 +1,26 @@
 package com.example.esteticacanina.adapter;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.esteticacanina.MascotaActivity;
 import com.example.esteticacanina.R;
 import com.example.esteticacanina.model.Pet;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.DocumentSnapshot;
 
 public class PetAdapter extends FirestoreRecyclerAdapter<Pet, PetAdapter.ViewHolder> {
+
+    Activity activity;
 
     /**
      * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
@@ -23,13 +30,26 @@ public class PetAdapter extends FirestoreRecyclerAdapter<Pet, PetAdapter.ViewHol
      */
     public PetAdapter(@NonNull FirestoreRecyclerOptions<Pet> options) {
         super(options);
+        this.activity = activity;
     }
 
     @Override
     protected void onBindViewHolder(@NonNull PetAdapter.ViewHolder holder, int position, @NonNull Pet pet) {
+        DocumentSnapshot documentSnapshot = getSnapshots().getSnapshot(holder.getAbsoluteAdapterPosition());
+        final String id = documentSnapshot.getId();
+
         holder.nombre.setText(pet.getNombre());
         holder.sexo.setText(pet.getSexo());
         holder.peso.setText(pet.getPeso());
+        holder.vermas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(activity, MascotaActivity.class);
+                i.putExtra("id_pet", id);
+                activity.startActivity(i);
+                activity.finish();
+            }
+        });
     }
 
     @NonNull
@@ -42,6 +62,7 @@ public class PetAdapter extends FirestoreRecyclerAdapter<Pet, PetAdapter.ViewHol
     public class ViewHolder extends RecyclerView.ViewHolder {
         //VARIABLES A RELLENAR
         TextView nombre, sexo, peso;
+        Button vermas;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -49,6 +70,7 @@ public class PetAdapter extends FirestoreRecyclerAdapter<Pet, PetAdapter.ViewHol
             nombre = itemView.findViewById(R.id.txtnompet);
             sexo = itemView.findViewById(R.id.txtsexo);
             peso = itemView.findViewById(R.id.txtpeso);
+            vermas = itemView.findViewById(R.id.btnvermas);
         }
     }
 }
